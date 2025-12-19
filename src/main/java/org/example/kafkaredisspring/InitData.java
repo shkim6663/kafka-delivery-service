070f83ec9562e61b604fa2c0f2de6d47e19891a7
Viva <happybird00@naver.com>
2025-12-19 15:45:39 +0900

    리팩토링

--- src/main/java/org/example/kafkaredisspring/InitData.java
+++
@@ -1,5 +0,0 @@
-package org.example.kafkaredisspring;
-
-public class InitData {
-
-}
--- src/main/java/org/example/kafkaredisspring/domain/product/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package org.example.kafkaredisspring.domain.product.repository;
-
-import org.example.kafkaredisspring.common.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-
-}
